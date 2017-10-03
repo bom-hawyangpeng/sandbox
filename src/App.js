d81ba8584d8735dc38370821757f097fb6f529a7
Haw Yang Peng <haw.peng@bom.gov.au>
2017-10-04 09:58:18 +1100

    Simple click grid cell game

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import logo from './logo.svg';
 import './App.css';
-import Button from './Button.js';
+
+const Button = styled.button`
+  background-color: ${props => props.highlight ? 'palevioletred' : 'lightgrey'};
+  margin: 10px;
+  height: 3em;
+  width: 3em;
+  font-size: 25px;
+  border: none;
+`;
+
+const Grid = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
 
 class App extends Component {
 
@@ -9,8 +31,46 @@ class App extends Component {
     super();
 
     this.state = {
-      count: 0
+      score: 0,
+      rowCount: 4,
+      columnCount: 4,
+      x: null,
+      y: null
     }
+  }
+
+  componentDidMount() {
+    this.selectRandomCell();
+  }
+
+  generateGrid() {
+    let rows = [];
+    for (let x = 0; x < this.state.rowCount; x++){
+      let cells = [];
+      for (let y = 0; y < this.state.columnCount; y++){
+        const highlight = x === this.state.x && y === this.state.y;
+        cells.push(<Button key={`cell${x}-${y}`} highlight={highlight} onClick={() => this.handleClick(highlight)}></Button>)
+      }
+      rows.push(<Row key={`row${x}`}>{cells}</Row>)
+    }
+    return rows;
+  }
+
+  handleClick(highlight) {
+    if ( highlight ) {
+      this.increaseScore(1);
+      this.selectRandomCell();
+    }
+  }
+
+  selectRandomCell() {
+    let randomX = Math.floor(Math.random() * this.state.rowCount);
+    let randomY = Math.floor(Math.random() * this.state.columnCount);
+    this.setState({ x: randomX, y: randomY })
+  }
+
+  increaseScore(score) {
+    this.setState({ score: this.state.score + score});
   }
 
   render() {
@@ -21,12 +81,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          {this.state.count}
+          Score: {this.state.score}
         </p>
-        <div className="ButtonContainer">
-          <Button onClick={() => this.setState({ count: this.state.count + 1 })}>+</Button>
-          <Button onClick={() => this.setState({ count: this.state.count - 1 })}>-</Button>
-        </div>
+        <Grid>
+          {this.generateGrid()}
+        </Grid>
       </div>
     );
   }
